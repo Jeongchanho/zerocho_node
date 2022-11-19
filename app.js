646c34dfc0c1aac50f20065d63ff9380e4a2b8f3
chanho <gowjr4jch010@naver.com>
2022-11-19 17:37:09 +0900

    11.19_morgan, cookieparser, 추가완료

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
 const express = require("express"); //express 불러오기
 const path = require("path"); //경로 설정
+const morgan = require("morgan"); //서버에 요청 응답기록
+const cookieParser = require("cookie-parser");
 
 const app = express(); //express app에 할당
 
 app.set("port", process.env.PORT || 3000); //포트할당
 
-app.use((req, res, next) => {
-  console.log("모든 요청에 실행하고싶어요");
-  next();
-});
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
+// 앞에는 요청경로 뒤에는 실제 경로
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //true면 qs, false면 querystring
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+  ㅇㅇ;
 });
 
 app.post("/", (req, res) => {
   res.send("hello express");
 });
 
+app.get("/category/Javascript", (req, res) => {
+  res.send("hello Javascript");
+});
+
 app.get("/about", (req, res) => {
   res.send("hello express");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.send("에러발생");
 });
 
 app.listen(app.get("port"), () => {
